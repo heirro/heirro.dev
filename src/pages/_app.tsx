282332eee556fc8c2a0d811c2101c00c097baecd
Vava Heirro <vava.heirro@impact360design.com>
2022-08-23 16:46:12 +0700

    Change library nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,30 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
 import { AppProps } from 'next/app';
-import NextNProgress from 'nextjs-progressbar';
+import Router from "next/router";
+import nProgress from "nprogress";
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
-config.autoAddCss = false;
+config.autoAddCss = true;
 
 import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
 import '@/styles/colors.css';
+import '@/styles/nprogress.css';
 
 /**
  * !STARTERCONF info
  * ? `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
  */
+ Router.events.on("routeChangeStart", nProgress.start);
+ Router.events.on("routeChangeError", nProgress.done);
+ Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <>
-    <NextNProgress 
-        color="#000000"
-    />
-    <Component {...pageProps} />;
-  </>
-  )
+    <>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
