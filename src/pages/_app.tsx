6545ae527002a160af8126f627db56d5d797c97f
Vava Heirro <vava.heirro@impact360design.com>
2022-08-23 19:50:59 +0700

    somefix nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
 import { AppProps } from 'next/app';
-import Router from "next/router";
+import { useRouter } from 'next/router';
 import nProgress from "nprogress";
+import { useEffect } from 'react';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
-config.autoAddCss = true;
+config.autoAddCss = false;
 
 import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
@@ -15,16 +16,16 @@ import '@/styles/nprogress.css';
  * !STARTERCONF info
  * ? `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
  */
- Router.events.on("routeChangeStart", nProgress.start);
- Router.events.on("routeChangeError", nProgress.done);
- Router.events.on("routeChangeComplete", nProgress.done);
-
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />;
-    </>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () =>  nProgress.start());
+    router.events.on('routeChangeComplete', () =>  nProgress.done());
+    router.events.on('routeChangeError', () =>  nProgress.done());
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
